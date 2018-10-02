5019eab78b13071c742c5e3d90445e878a11fb22
powershopcloud <creativo2b2c@powershop.es>
2018-10-02 10:47:38 +0200

    1

--- src/android/itos/device/plugin/ItosDevice.java
+++ src/android/itos/device/plugin/ItosDevice.java
@@ -140,7 +140,7 @@ public class ItosDevice extends CordovaPlugin {
                 callbackContext.error("User did not specify data to print");
                 return true;
             }
-            mLed.setLed(LightMode.get(1), true);
+            //mLed.setLed(LightMode.get(1), true);
             mPrinter = new Printer(this.cordova.getActivity().getBaseContext());
             mPrinter.initPrinter();
 
@@ -157,7 +157,7 @@ public class ItosDevice extends CordovaPlugin {
                     //showMessage( String.format( "PrintResult: %d", retCode ) );
                 }
             } );
-            mLed.setLed(LightMode.get(1), false);
+            //mLed.setLed(LightMode.get(1), false);
         } else {
             return false;
         }
